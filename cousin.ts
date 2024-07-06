e3a9db5100f007aa32bc6b8bf2038b74701eeae5
JoaoGabrielPereiraLopes <147850438+JoaoGabrielPereiraLopes@users.noreply.github.com>
2024-07-06 20:43:39 -0300

    Add files via upload

---
+++ cousin.ts
@@ -0,0 +1,10 @@
+function main(Number:number):boolean{
+    let cousin=true;//this represent if the number is cousin
+    for(let x=2;x**2<Number;x++){
+        if(Number%x==0){
+            cousin=false;
+        }
+    }
+    return cousin;
+}
+console.log(main(2));
