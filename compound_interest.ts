3717e523a986aae20bdc13f2f9e944b932f315de
JoaoGabrielPereiraLopes <147850438+JoaoGabrielPereiraLopes@users.noreply.github.com>
2024-07-29 22:13:43 -0300

    Delete compound_interest.ts

--- compound_interest.ts
+++
@@ -1,8 +0,0 @@
-function main(months:number,rate:number,money:number,payment:number):number{
-    for(let x=0;x<months;x++){
-        money*=1+rate/100;
-        money+=payment
-    }
-    return money;
-}
-console.log(main(10,25,1000,0));
